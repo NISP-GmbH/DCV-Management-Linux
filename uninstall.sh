656724bb36e48ae6091b00eec206d2936ebba85b
Francisco Panis Kaseker <franciscopk@ni-sp.com>
2024-05-31 09:16:58 -0300

    add variable and missing token dir

--- uninstall.sh
+++ uninstall.sh
@@ -8,14 +8,15 @@ sudo systemctl disable dcv-management.service
 
 # remove the services directories
 sudo rm -rf $dcv_management_dir
-sudo rm -rf $dcv_management_dir
+sudo rm -rf $dcv_tokens_path
 
 # remove scripts
 sudo rm -f /usr/bin/dcv_get_token
 sudo rm -f /usr/bin/dcv_tokens_check
 sudo rm -f /usr/bin/dcv_local_sessions
 sudo rm -f /usr/bin/dcv_local_sessions_timedout
-sudo rm -f /etc/pam.d/dcv.custom
+sudo rm -f $dcv_management_conf_path
+sudo rm -f $dcv_pamd_file_conf
 sudo sed '/^pam-service-name=.*$/d' /etc/dcv/dcv.conf
 sudo sed '/^auth-token-verifier=.*$/d' /etc/dcv/dcv.conf
 
