73e5dd147e392701f51a4ed6aabe13c5f26c2f39
Francisco Panis Kaseker <franciscopk@ni-sp.com>
2025-01-23 01:05:42 +0100

    add more files

--- create_tarball_installer.sh
+++ create_tarball_installer.sh
@@ -2,7 +2,7 @@
 
 temp_dir="dcv_local_sessions_install"
 mkdir $temp_dir
-cp -a install.sh uninstall.sh ${temp_dir}/
+cp -a install.sh uninstall.sh dcv_collab_prompt_script.sh config.conf library.sh ${temp_dir}/
 mkdir ${temp_dir}/api/
 cp -a api/app.py ${temp_dir}/api/
 tar czf ${temp_dir}.tar.gz $temp_dir
