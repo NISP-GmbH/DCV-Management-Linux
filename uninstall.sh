ba4ea05d6c2aab61de58adac42657236efe8b007
Francisco Panis Kaseker <franciscopk@ni-sp.com>
2025-01-23 01:06:02 +0100

    remove extra file

--- uninstall.sh
+++ uninstall.sh
@@ -15,6 +15,7 @@ sudo rm -f /usr/bin/dcv_get_token
 sudo rm -f /usr/bin/dcv_tokens_check
 sudo rm -f /usr/bin/dcv_local_sessions
 sudo rm -f /usr/bin/dcv_local_sessions_timedout
+sudo rm -f $dcv_collab_prompt_script
 sudo rm -f $dcv_management_conf_path
 sudo rm -f $dcv_pamd_file_conf
 sudo sed '/^pam-service-name=.*$/d' /etc/dcv/dcv.conf
